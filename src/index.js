f7b87c3cb0d36f4322c3401702b2209c4e51d568
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2021-11-07 17:47:20 +0700

    Set up sagaMiddleware

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import createSagaMiddleware from "redux-saga";
+
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(mySaga);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
